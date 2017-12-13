020b8e90bc2eec827b84352da27fdd1f17fa56e7
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 10:38:57 +0100

    ajout exo7

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>exercice 6 partie 3 php</title>
-  </head>
-  <body>
-    <?php
-    $text = 'On tient le bon bout';
-      for ($number = 1; $number <= 100; $number+=15) {
-        echo $text.'<br/>';
-      }
-     ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>exercice 7 partie 7 php</title>
+</head>
+<body>
+  <p><?php
+  if (isset($_POST['civility']) && ($_POST['lastname']) && ($_POST['firstname']) && ($_POST['myFile'])) {
+    echo 'Bonjour ' . $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'] . '. Votre fichier est : ' . $_POST['myFile'];
+  } else {
+    ?>
+    <form class="form" action="index.php" method="post" enctype="multipart/form-data">
+      <select class="civility" name="civility">
+        <option value="Monsieur" name="Monsieur">Monsieur</option>
+        <option value="Madame" name="Madame">Madame</option>
+      </select>
+      <input type="lastname" name="lastname" placeholder="Nom">
+      <input type="firstname" name="firstname" placeholder="Prénom">
+      <input type="file" name="myFile">
+      <button type="submit" name="validate">Valider</button>
+    </form>
+  <?php }
+  ?>
+</p>
+</body>
 </html>
